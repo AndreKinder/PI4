1a2cf05951229e37406277b10c4acecc1a5c4534
AndreKinder <andre.kinder@gmail.com>
2016-06-11 12:57:54 -0300

    Botão voltar travado

--- Quiz/app/src/main/java/br/com/diehard/quiz/JogoVerdadeiro.java
+++ Quiz/app/src/main/java/br/com/diehard/quiz/JogoVerdadeiro.java
@@ -4,9 +4,9 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.support.annotation.BoolRes;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -186,6 +186,15 @@ public class JogoVerdadeiro extends AppCompatActivity {
 
             progress.dismiss();
         }
+    }
+    @Override
+    public boolean onKeyDown(int keycode, KeyEvent event)
+    {
+        if(keycode == KeyEvent.KEYCODE_BACK)
+        {
+            return true;
+        }
+        return super.onKeyDown(keycode, event);
     }
 
 }
